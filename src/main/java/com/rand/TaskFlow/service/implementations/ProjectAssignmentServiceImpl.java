8ef579f489c8647a12f6ce7e4e78e74a4d059af5
randH7 <rand_alharthi@hotmail.com>
2023-11-17 22:57:17 +0300

    DONE: project controller

--- src/main/java/com/rand/TaskFlow/service/implementations/ProjectAssignmentServiceImpl.java
+++ src/main/java/com/rand/TaskFlow/service/implementations/ProjectAssignmentServiceImpl.java
@@ -71,7 +71,6 @@ public class ProjectAssignmentServiceImpl implements ProjectAssignmentService {
                 .collect(Collectors.toList());
     }
 
-
     @Override
     public List<String> findByProjectOrderByEmploy(Integer projectId) {
         return projectAssignmentRepo.findByProjectOrderByEmploy(projectId);
